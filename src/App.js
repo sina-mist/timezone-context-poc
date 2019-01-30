28f8218d443fc4021b0c95f380790ad3411b6402
Sina Pinto <spinto@mist.com>
2019-01-30 12:41:48 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Child from "./Child";
+import ChildB from "./ChildB";
+import "./App.css";
+
+export const TimezoneContext = React.createContext();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.setTimezone = tz => {
+      // update moment
+      // moment().tz(tz)
+
+      // trigger re-render of context consumers
+      this.setState(prevState => ({
+        timezoneContext: {
+          ...prevState.timezoneContext,
+          timezone: tz
+        }
+      }));
+    };
+
+    // we hold the context in state in order to avoid a gotcha where
+    // all context consumers re-render whenever this App component re-renders
+    // because React does a reference check on the `value` we pass to the provider
+    // and if we were to inline an object in `value` it would create a new reference
+    // whenever App re-renders.
+    this.state = {
+      timezoneContext: {
+        timezone: "Pacific",
+        setTimezone: this.setTimezone
+      }
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TimezoneContext.Provider value={this.state.timezoneContext}>
+          <Child />
+          <ChildB />
+        </TimezoneContext.Provider>
       </div>
     );
   }
