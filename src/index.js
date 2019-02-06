9cae3813fe2a882cf2c979ffe35174debb8ab106
Samuel Crank <sjcrank@gmail.com>
2019-02-06 15:11:20 -0500

    update docs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TimezoneApp from "./context/TimezoneApp";
+// import TimezoneApp from "./model/TimezoneApp";
 
 ReactDOM.render(<TimezoneApp />, document.getElementById("root"));
